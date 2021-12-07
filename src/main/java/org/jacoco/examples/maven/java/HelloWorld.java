0941a5485e079cac98bb10d7a9c775b8889e697e
BrahmaReddyK9 <94994750+BrahmaReddyK9@users.noreply.github.com>
2021-12-07 11:43:28 +0530

    Update HelloWorld.java

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -3,7 +3,7 @@ package org.jacoco.examples.maven.java;
 public class HelloWorld {
 
 	public String getMessage(boolean bigger) {
-		if (bigger) {
+		if (bigger) {,,,
 			return "Hello Universe!";
 		} else {
 			return "Hello World!";
